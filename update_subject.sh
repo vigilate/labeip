c5b4c79acd82334b5bf1777249114ce9f43902e9
Kevin Soules <kevin@libdev.so>
2015-02-09 16:00:22 +0100

    Add subject directory and script to update it

---
+++ update_subject.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+## update_subject.sh for  in /home/eax/dev/vigilate/labeip
+## 
+## Made by eax
+## Login   <soules_k@epitech.net>
+## 
+## Started on  Mon Feb  9 15:55:30 2015 eax
+## Last update Mon Feb  9 15:58:27 2015 eax
+##
+
+
+## If you want svn to use your epitech username (on linux)
+## add theses line to your ~/.subversion/servers :
+##
+## [groups]
+## epitecheip = labeip.epitech.eu
+##
+## [epitecheip]
+## username = logi_n
+
+svn co https://labeip.epitech.eu/svn/eip/public/2017/ subjects
+
